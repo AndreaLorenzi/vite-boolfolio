1cd59a3de8c8ac9e2c0f9e9b5f22f789b7dd3fd5
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-17 01:32:56 +0200

    da finire ma fatto la base del lavoro

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,16 +1,37 @@
 <script>
-    export default {
-
+import axios from "axios";
+export default {
+  data() {
+    return {
+      arrProjects: [],
+      page: 1, // eventuale parametro
     };
+  },
 
+  mounted: {
+    getProjects() {
+      axios
+        // oppure link di api esterne
+        .get("http://localhost:8000/api/projects", {
+          // eventuali parametri
+          params: {
+            page: this.page,
+          },
+        })
+        .then((response) => (this.arrProjects = response.data));
+    },
+  },
+  created() {
+    this.getProjects();
+  },
+};
 </script>
 
 <template>
-
-
+  <div v-for="project in arrProjects" :key="project.id">
+    {{ project.title }}
+  </div>
+  <div></div>
 </template>
 
-<style lang=scss scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
