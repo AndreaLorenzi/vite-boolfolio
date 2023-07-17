67f6f41d4c37397a7795538df0000b79cc453225
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-17 17:05:37 +0200

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import ProjectList from "./components/ProjectList.vue";
+import ProjectNav from "./components/ProjectNav.vue";
 
 export default {
   components: {
     ProjectList,
+    ProjectNav,
   },
 };
 </script>
 
 <template>
+  <ProjectNav />
   <router-view></router-view>
   <ProjectList />
 </template>
